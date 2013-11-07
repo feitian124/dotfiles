c92a24b7f48479610c827617938be5b8491374c5
feitian124 <feitian124@gmail.com>
2013-11-07 23:10:39 +0800

    uncomment git command

--- install.sh
+++ install.sh
@@ -4,17 +4,19 @@ folder="$HOME/dotfiles"
 
 if [ ! -d "$folder" ]; then
     echo "Installing dotfiles in $folder"
-    #git clone https://github.com/feitian124/dotfiles.git "$folder"
+    git clone https://github.com/feitian124/dotfiles.git "$folder"
 else
     echo "dotfiles is already installed"
 fi
 
-echo "\n------------- backup dotfiles -------------------"
+echo ""
+echo "------------- backup dotfiles -------------------"
 mv -v "$HOME/.bashrc"       "/tmp/bashrc"
 mv -v "$HOME/.vimrc"        "/tmp/vimrc"
 mv -v "$HOME/.tmux.conf"    "/tmp/tmux.conf"
 
-echo "\n------------- link   dotfiles -------------------"
+echo ""
+echo "------------- link dotfiles -------------------"
 ln -sv "$folder/bashrc"      "$HOME/.bashrc"
 ln -sv "$folder/vimrc"       "$HOME/.vimrc"
 ln -sv "$folder/tmux.conf"   "$HOME/.tmux.conf"
