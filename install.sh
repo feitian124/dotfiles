a5cae011c30cc7581e555a0234fda348ec29d47b
feitian124 <feitian124@gmail.com>
2013-11-07 23:31:00 +0800

    group script code by vim, bash, tmux

--- install.sh
+++ install.sh
@@ -6,17 +6,23 @@ if [ ! -d "$folder" ]; then
     echo "Installing dotfiles in $folder"
     git clone https://github.com/feitian124/dotfiles.git "$folder"
 else
-    echo "dotfiles is already installed"
+    echo "dotfiles is already installed, try delete $folder then re-run"
 fi
 
 echo ""
-echo "------------- backup dotfiles -------------------"
+echo "------------- bash dotfile -------------------"
 mv -v "$HOME/.bashrc"       "/tmp/bashrc"
-mv -v "$HOME/.vimrc"        "/tmp/vimrc"
-mv -v "$HOME/.tmux.conf"    "/tmp/tmux.conf"
+ln -sv "$folder/bashrc"      "$HOME/.bashrc"
 
 echo ""
-echo "------------- link dotfiles -------------------"
-ln -sv "$folder/bashrc"      "$HOME/.bashrc"
-ln -sv "$folder/vimrc"       "$HOME/.vimrc"
+echo "------------- vim dotfile -------------------"
+mv -v "$HOME/.vimrc"              "/tmp/vimrc"
+mv -v "$HOME/.vim/bundle/vundle"  "/tmp/vundle"
+ln -sv "$folder/vimrc"      "$HOME/.vimrc"
+git clone https://github.com/gmarik/vundle.git "$HOME/.vim/bundle/vundle"
+vim +BundleInstall +qall
+
+echo ""
+echo "------------- tmux dotfile -------------------"
+mv -v "$HOME/.tmux.conf"    "/tmp/tmux.conf"
 ln -sv "$folder/tmux.conf"   "$HOME/.tmux.conf"
